6657d3b553c9ac27a698aa26d03d268682726699
amithamemit2 <amith@ofakim.tzafonet.org.il>
2022-04-11 16:19:04 +0300

    added variable validity confirmation

--- SignPage_AmitHarit/SignPage_AmitHarit/WebForm1.aspx.cs
+++ SignPage_AmitHarit/SignPage_AmitHarit/WebForm1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
 
 namespace SignPage_AmitHarit
 {
@@ -22,7 +23,54 @@ namespace SignPage_AmitHarit
                 string conPass = Request.Form["conPass"];
                 string gender = Request.Form["gender"];
                 string birthYear = Request.Form["birthYear"];
+
+                
             }
+        }
+
+        /*
+         * this function checks if email is valid, id is valid and if password is valid (contains uppercase, lowerse, numeric and special characters and its length is bigger than 8)
+         * input: email, user id and password
+         * ouput: bool (valid or not)
+        */
+        private static bool validParameters(string email, string id, string password)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false; // suggested by @TK-421
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+
+            int tmpPass;
+            if (id.Length != 9 || !int.TryParse(id, out tmpPass))
+            {
+                return false;
+            }
+
+
+            string regex = "^(?=.*[a-z])(?=."
+                    + "*[A-Z])(?=.*\\d)"
+                    + "(?=.*[-+_!@#$%^&*., ?]).+$";
+
+            Regex p = new Regex(regex);
+
+            Match m = p.Match(password); //using regex to check if password contains uppercase, lowerse, numeric and special characters
+
+
+            if (!m.Success || password.Length <= 8)
+                return false;
+
+            return true;
         }
     }
 }
---
+++ SignPage_AmitHarit/SignPage_AmitHarit/helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SignPage_AmitHarit
+{
+    public class helper
+    {
+        public static string CalculateSha256Hash(string input)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static bool CompareHashes(string firstHash, string secondHash)
+        {
+            return SHA256.Equals(firstHash, secondHash);
+        }
+    }
+}
